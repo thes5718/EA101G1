a4e12550e0fd863191bd120bd93fc0267b6c9bc8
thes5718 <59568024+thes5718@users.noreply.github.com>
2020-06-11 16:09:47 +0800

    add product_type

--- EA101G1/src/com/productTyep/model/PtDAO.java
+++ EA101G1/src/com/productTyep/model/PtDAO.java
@@ -1,37 +1,239 @@
 package com.productTyep.model;
 
-import java.util.List;
+import java.util.*;
+import java.sql.*;
 
 public class PtDAO {
-	
+
 	String driver = "oracle.jdbc.driver.OracleDriver";
 	String url = "jdbc:oracle:thin:@localhost:1521:XE";
 	String userid = "DANNY";
 	String passwd = "123456";
-	
+
 	private static final String INSERT = "INSERT INTO PRODUCT_TYPE(PT_ID,TYPENAME) VALUES(?,?)";
-	private static final String UPDATE = "UPDATE PRODUCT_TYPE SET PT_ID=?,TYPENAME=? WHERE PT_ID=?";
+	private static final String UPDATE = "UPDATE PRODUCT_TYPE SET TYPENAME=? WHERE PT_ID=?";
 	private static final String DELETE = "DELETE FROM PRODUCT_TYPE WHERE PT_ID=?";
 	private static final String GET_ALL_STMT = "SELECT PT_ID,TYPENAME FROM PRODUCT_TYPE ORDER BY PT_ID";
 	private static final String GET_ONE_STMT = "SELECT PT_ID,TYPENAME FROM PRODUCT_TYPE WHERE PT_ID=?";
-	
-	public void insert(PtVO proVO) {
+
+	public void insert(PtVO ptVO) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
 		
-		
+		try {
+			
+			Class.forName(driver);
+			con = DriverManager.getConnection(url,userid,passwd);
+			pstmt = con.prepareStatement(INSERT);
+			
+			pstmt.setString(1,ptVO.getPt_id());
+			pstmt.setString(2,ptVO.getTypename());
+			
+			pstmt.executeUpdate();
+			
+		}catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. " + e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. " + se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
 	}
 	
-	public void update(PtVO proVO) {}
-	
-	public void delete(String p_id) {}
-	
-	public PtVO findByPrimaryKey(String pt_id ) {
+	public void update(PtVO ptVO) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
 		
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url,userid,passwd);
+			pstmt = con.prepareStatement(UPDATE);
+			
+			pstmt.setString(1,ptVO.getTypename());
+			pstmt.setString(2,ptVO.getPt_id());
+			
+			pstmt.executeUpdate();
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. " + e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. " + se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+
+	}
+
+	public void delete(String pt_id) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		try { 
+			Class.forName(driver);
+			con = DriverManager.getConnection(url,userid,passwd);
+			pstmt = con.prepareStatement(DELETE);
+			
+			pstmt.setString(1,pt_id);
+			
+			pstmt.executeUpdate();
+		}catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. " + e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. " + se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+	}
+
+	public PtVO findByPrimaryKey(String pt_id) {
+		PtVO ptVO = null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url,userid,passwd);
+			pstmt = con.prepareStatement(GET_ONE_STMT);
+			
+			pstmt.setString(1, pt_id);
+			rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				
+				ptVO = new PtVO();
+				ptVO.setPt_id(rs.getString("pt_id"));
+				ptVO.setTypename(rs.getString("typename"));
+		
+			}
+		}catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. " + e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. " + se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		return ptVO;
 		
 	}
-	
-	public List<PtVO> getAll(){
+
+	public List<PtVO> getAll() {
+		List <PtVO> list = new ArrayList<PtVO>();
+		PtVO ptVO = null;
 		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 		
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url,userid,passwd);
+			pstmt = con.prepareStatement(GET_ALL_STMT);
+			rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				
+				ptVO = new PtVO();
+				ptVO.setPt_id(rs.getString("pt_id"));
+				ptVO.setTypename(rs.getString("typename"));
+				list.add(ptVO);
+			}
+		}catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. " + e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. " + se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		return list;
 	}
-	
+
 }
---
+++ EA101G1/src/test/test2.java
@@ -0,0 +1,86 @@
+package test;
+
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.productTyep.model.PtDAO;
+import com.productTyep.model.PtVO;
+
+
+public class test2 {
+
+	public static void main(String[] args) {
+
+		PtDAO dao = new PtDAO();
+//		
+////			
+//			PtVO ptVO1 = new PtVO();
+//			ptVO1.setPt_id("test");
+//			ptVO1.setTypename("測試");
+//			dao.insert(ptVO1);
+//			System.out.println("新增成功");
+		
+
+//		// 修改
+//
+//			
+//			PtVO ptVO1 = new PtVO();
+//			ptVO1.setPt_id("test");
+//			ptVO1.setTypename("weeeeeeeeeeeeeeeeeeeed");
+//			
+//			dao.update(ptVO1);
+//			System.out.println("更新成功");
+//		
+//
+//		// 刪除
+//		dao.delete("test");
+//
+//		// 查詢
+//		PtVO ptVO1 = dao.findByPrimaryKey("test");
+//		System.out.print(ptVO1.getPt_id() + ",");
+//		System.out.print(ptVO1.getTypename() + ",");
+//		System.out.println("---------------------");
+//
+//		// 查詢
+		List<PtVO> list = dao.getAll();
+		for (PtVO pro : list) {
+			System.out.print(pro.getPt_id() + ",");
+			System.out.println(pro.getTypename());
+			System.out.println("---------------------");
+			System.out.println();
+		}
+	}
+
+	public static InputStream getPictureStream(String path) throws IOException {
+		File file = new File(path);
+		FileInputStream fis = new FileInputStream(file);
+		return fis;
+	}
+
+	// 使用byte[]方式
+	public static byte[] getPictureByteArray(String path) throws IOException {
+		File file = new File(path);
+		FileInputStream fis = new FileInputStream(file);
+		ByteArrayOutputStream baos = new ByteArrayOutputStream(); // 輸出到系統預設的byte陣列
+		byte[] buffer = new byte[8192];
+		int i;
+		while ((i = fis.read(buffer)) != -1) {
+			baos.write(buffer, 0, i);
+		}
+		baos.close();
+		fis.close();
+
+		return baos.toByteArray(); // 回傳圖片位元資料
+	}
+}
